c716ac62828ca70ba52e53e4fa0c415229bc820d
itcosplay <azure_dream@mail.ru>
2021-12-20 15:53:00 +0300

    Fixed  admin pages load time

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author', 'likes', 'tags')
+    list_display = ('title', 'published_at')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    raw_id_fields = ('post', 'author')
+    list_display = ('published_at', 'author')
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
